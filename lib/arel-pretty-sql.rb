c333d583e565442c9c916f95bb065db8107e038f
Erik Fonselius <fonsan@gmail.com>
2011-05-27 10:05:30 +0200

    Added methods to override

--- lib/arel-pretty-sql.rb
+++ lib/arel-pretty-sql.rb
@@ -11,6 +11,296 @@ module Arel
   
   module Visitors
     class ToSqlPretty < ToSql
+      def visit_Arel_Nodes_DeleteStatement o
+        [
+          "DELETE FROM #{visit o.relation}",
+          ("WHERE #{o.wheres.map { |x| visit x }.join ' AND '}" unless o.wheres.empty?)
+        ].compact.join ' '
+      end
+      
+      def visit_Arel_Nodes_UpdateStatement o
+        if o.orders.empty? && o.limit.nil?
+          wheres = o.wheres
+        else
+          key = o.key
+          unless key
+            warn(<<-eowarn) if $VERBOSE
+(#{caller.first}) Using UpdateManager without setting UpdateManager#key is
+deprecated and support will be removed in ARel 3.0.0.  Please set the primary
+key on UpdateManager using UpdateManager#key=
+            eowarn
+            key = o.relation.primary_key
+          end
+
+          wheres = [Nodes::In.new(key, [build_subselect(key, o)])]
+        end
+
+        [
+          "UPDATE #{visit o.relation}",
+          ("SET #{o.values.map { |value| visit value }.join ', '}" unless o.values.empty?),
+          ("WHERE #{wheres.map { |x| visit x }.join ' AND '}" unless wheres.empty?),
+        ].compact.join ' '
+      end
+      
+      def visit_Arel_Nodes_InsertStatement o
+        [
+          "INSERT INTO #{visit o.relation}",
+
+          ("(#{o.columns.map { |x|
+          quote_column_name x.name
+        }.join ', '})" unless o.columns.empty?),
+
+          (visit o.values if o.values),
+        ].compact.join ' '
+      end
+      
+      def visit_Arel_Nodes_Exists o
+        "EXISTS (#{visit o.expressions})#{
+          o.alias ? " AS #{visit o.alias}" : ''}"
+      end
+      
+      def visit_Arel_Nodes_Values o
+        "VALUES (#{o.expressions.zip(o.columns).map { |value, attr|
+          if Nodes::SqlLiteral === value
+            visit_Arel_Nodes_SqlLiteral value
+          else
+            quote(value, attr && column_for(attr))
+          end
+        }.join ', '})"
+      end
+      
+      def visit_Arel_Nodes_SelectStatement o
+        [
+          (visit(o.with) if o.with),
+          o.cores.map { |x| visit_Arel_Nodes_SelectCore x }.join,
+          ("ORDER BY #{o.orders.map { |x| visit x }.join(', ')}" unless o.orders.empty?),
+          (visit(o.limit) if o.limit),
+          (visit(o.offset) if o.offset),
+          (visit(o.lock) if o.lock),
+        ].compact.join ' '
+      end
+
+      def visit_Arel_Nodes_SelectCore o
+        [
+          "SELECT",
+          (visit(o.top) if o.top),
+          (visit(o.set_quantifier) if o.set_quantifier),
+          ("#{o.projections.map { |x| visit x }.join ', '}" unless o.projections.empty?),
+          (visit(o.source) if o.source),
+          ("WHERE #{o.wheres.map { |x| visit x }.join ' AND ' }" unless o.wheres.empty?),
+          ("GROUP BY #{o.groups.map { |x| visit x }.join ', ' }" unless o.groups.empty?),
+          (visit(o.having) if o.having),
+        ].compact.join ' '
+      end
+      
+      def visit_Arel_Nodes_Distinct o
+        'DISTINCT'
+      end
+      
+      def visit_Arel_Nodes_With o
+        "WITH #{o.children.map { |x| visit x }.join(', ')}"
+      end
+
+      def visit_Arel_Nodes_WithRecursive o
+        "WITH RECURSIVE #{o.children.map { |x| visit x }.join(', ')}"
+      end
+
+      def visit_Arel_Nodes_Union o
+        "( #{visit o.left} UNION #{visit o.right} )"
+      end
+
+      def visit_Arel_Nodes_UnionAll o
+        "( #{visit o.left} UNION ALL #{visit o.right} )"
+      end
+
+      def visit_Arel_Nodes_Intersect o
+        "( #{visit o.left} INTERSECT #{visit o.right} )"
+      end
+
+      def visit_Arel_Nodes_Except o
+        "( #{visit o.left} EXCEPT #{visit o.right} )"
+      end
+
+      def visit_Arel_Nodes_Having o
+        "HAVING #{visit o.expr}"
+      end
+
+      def visit_Arel_Nodes_Offset o
+        "OFFSET #{visit o.expr}"
+      end
+
+      def visit_Arel_Nodes_Limit o
+        "LIMIT #{visit o.expr}"
+      end
+      
+      def visit_Arel_Nodes_Ordering o
+        "#{visit o.expr} #{o.descending? ? 'DESC' : 'ASC'}"
+      end
+      
+      def visit_Arel_Nodes_NamedFunction o
+        "#{o.name}(#{o.distinct ? 'DISTINCT ' : ''}#{o.expressions.map { |x|
+          visit x
+        }.join(', ')})#{o.alias ? " AS #{visit o.alias}" : ''}"
+      end
+      
+      def visit_Arel_Nodes_Count o
+        "COUNT(#{o.distinct ? 'DISTINCT ' : ''}#{o.expressions.map { |x|
+          visit x
+        }.join(', ')})#{o.alias ? " AS #{visit o.alias}" : ''}"
+      end
+
+      def visit_Arel_Nodes_Sum o
+        "SUM(#{o.expressions.map { |x|
+          visit x }.join(', ')})#{o.alias ? " AS #{visit o.alias}" : ''}"
+      end
+
+      def visit_Arel_Nodes_Max o
+        "MAX(#{o.expressions.map { |x|
+          visit x }.join(', ')})#{o.alias ? " AS #{visit o.alias}" : ''}"
+      end
+
+      def visit_Arel_Nodes_Min o
+        "MIN(#{o.expressions.map { |x|
+          visit x }.join(', ')})#{o.alias ? " AS #{visit o.alias}" : ''}"
+      end
+
+      def visit_Arel_Nodes_Avg o
+        "AVG(#{o.expressions.map { |x|
+          visit x }.join(', ')})#{o.alias ? " AS #{visit o.alias}" : ''}"
+      end
+
+      def visit_Arel_Nodes_TableAlias o
+        "#{visit o.relation} #{quote_table_name o.name}"
+      end
+
+      def visit_Arel_Nodes_Between o
+        "#{visit o.left} BETWEEN #{visit o.right}"
+      end
+
+      def visit_Arel_Nodes_GreaterThanOrEqual o
+        "#{visit o.left} >= #{visit o.right}"
+      end
+
+      def visit_Arel_Nodes_GreaterThan o
+        "#{visit o.left} > #{visit o.right}"
+      end
+
+      def visit_Arel_Nodes_LessThanOrEqual o
+        "#{visit o.left} <= #{visit o.right}"
+      end
+
+      def visit_Arel_Nodes_LessThan o
+        "#{visit o.left} < #{visit o.right}"
+      end
+
+      def visit_Arel_Nodes_Matches o
+        "#{visit o.left} LIKE #{visit o.right}"
+      end
+
+      def visit_Arel_Nodes_DoesNotMatch o
+        "#{visit o.left} NOT LIKE #{visit o.right}"
+      end
+
+      def visit_Arel_Nodes_JoinSource o
+        return unless o.left || !o.right.empty?
+
+        [
+          "FROM",
+          (visit(o.left) if o.left),
+          o.right.map { |j| visit j }.join(' ')
+        ].compact.join ' '
+      end
+
+      def visit_Arel_Nodes_StringJoin o
+        visit o.left
+      end
+
+      def visit_Arel_Nodes_OuterJoin o
+        "LEFT OUTER JOIN #{visit o.left} #{visit o.right}"
+      end
+
+      def visit_Arel_Nodes_InnerJoin o
+        "INNER JOIN #{visit o.left} #{visit o.right if o.right}"
+      end
+
+      def visit_Arel_Nodes_On o
+        "ON #{visit o.expr}"
+      end
+
+      def visit_Arel_Nodes_Not o
+        "NOT (#{visit o.expr})"
+      end
+
+      def visit_Arel_Table o
+        if o.table_alias
+          "#{quote_table_name o.name} #{quote_table_name o.table_alias}"
+        else
+          quote_table_name o.name
+        end
+      end
+
+      def visit_Arel_Nodes_In o
+        "#{visit o.left} IN (#{visit o.right})"
+      end
+
+      def visit_Arel_Nodes_NotIn o
+        "#{visit o.left} NOT IN (#{visit o.right})"
+      end
+
+      def visit_Arel_Nodes_And o
+        o.children.map { |x| visit x }.join ' AND '
+      end
+
+      def visit_Arel_Nodes_Or o
+        "#{visit o.left} OR #{visit o.right}"
+      end
+
+      def visit_Arel_Nodes_Assignment o
+        right = quote(o.right, column_for(o.left))
+        "#{visit o.left} = #{right}"
+      end
+
+      def visit_Arel_Nodes_Equality o
+        right = o.right
+
+        if right.nil?
+          "#{visit o.left} IS NULL"
+        else
+          "#{visit o.left} = #{visit right}"
+        end
+      end
+
+      def visit_Arel_Nodes_NotEqual o
+        right = o.right
+
+        if right.nil?
+          "#{visit o.left} IS NOT NULL"
+        else
+          "#{visit o.left} != #{visit right}"
+        end
+      end
+
+      def visit_Arel_Nodes_As o
+        "#{visit o.left} AS #{visit o.right}"
+      end
+
+      def visit_Arel_Nodes_UnqualifiedColumn o
+        "#{quote_column_name o.name}"
+      end
+
+      def visit_Arel_Attributes_Attribute o
+        self.last_column = column_for o
+        join_name = o.relation.table_alias || o.relation.name
+        "#{quote_table_name join_name}.#{quote_column_name o.name}"
+      end
+      
+      def visit_Arel_Nodes_InfixOperation o
+        "#{visit o.left} #{o.operator} #{visit o.right}"
+      end
+      
+      def visit_Array o
+        o.empty? ? 'NULL' : o.map { |x| visit x }.join(', ')
+      end
       
     end
   end
